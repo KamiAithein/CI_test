03e1b7c1f1744bce4f0c5cbf29e76b9cc1c5c350
KamiAithein <k3nnethwkoepcke@gmail.com>
2021-05-21 16:19:58 -0500

    turned into application

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,7 @@
-fun main() {
-    val a = UAdder(32)
-    println(a.add(1U, 7U))
-
+fun main(args: Array<String>) {
+    try {
+        println(UAdder(32).add(args[0].toInt().toUInt(), args[1].toInt().toUInt()))
+    } catch(nfe: NumberFormatException) {
+        println("invalid params ${args.joinToString()}")
+    }
 }
